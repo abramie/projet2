a241fab8dc2da6eb43a0ebde9f600c750284c24e
jeremy <jeremyrou@hotmail.fr>
2017-10-25 15:08:18 +0200

    truc

--- proj2/main.c
+++ proj2/main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	pipe(tube);
 	pid_fils = fork();
 
-	if(argc >= 3 ) //verifie qu'on a bien le bon nombre d'argument
+	//if(argc >= 2 ) //verifie qu'on a bien le bon nombre d'argument
 	{
 		if(pid_fils != 0 ) //Pere (Emetteur ) 
 		{
@@ -36,11 +36,13 @@ int main(int argc, char *argv[])
 					message[i] = fgetc(f);		
 				}
 				
+				//while(fgets(message,taille,f)!= NULL);	
 				//Gestion envoie pipe
 				//Envoie des instructions : taille, valeur paramaetre, tableau
 				write(tube[1], &taille, sizeof(taille));
 				write(tube [1],argv[2], sizeof(char));
 				write(tube[1],message, taille*sizeof(char));
+				printf("\nemission: %s", message);
 				wait(NULL);
 				
 				//Fermeture fichier + pipe
@@ -58,57 +60,61 @@ int main(int argc, char *argv[])
 			//Receptions des infos
 			read(tube[0],&taille,sizeof(taille));
 			read(tube[0],&mode, sizeof(char));
-			printf("%c", mode);
 			message = (char*)malloc(taille * sizeof(char));
 			read(tube[0], message, taille*sizeof(char));
 			
 			int nbvoyelle;
-			
+			printf("\nreception : %s", message);
 			switch(mode ) //Opperations sur le message
 			{
-				case 1:
-				tableau2= (char*)malloc(taille * sizeof(char));
-				for(int i =0; i< taille; i++)
-				{
-					tableau2[i] = message[taille-i] ;
-				}
+				case '1':
+					tableau2= (char*)malloc(taille * sizeof(char));
+					for(int i =0; i< taille; i++)
+					{
+						tableau2[i] = message[taille-i] ;
+						printf("%c", tableau2[i]);
+					}
+					printf("1");
 				break;
 				
-				case 2:
+				case '2':
 				
-				nbvoyelle = 0;
-				for(int i =0; i< taille; i++)
-				{
-					if(message[i] == 'a' || message[i] == 'e' || 
-					message[i] == 'i' || message[i] == 'o' || 
-					message[i] == 'u' || message[i] == 'y')
+					nbvoyelle = 0;
+					for(int i =0; i< taille; i++)
 					{
-						nbvoyelle ++;			
+						if(message[i] == 'a' || message[i] == 'e' || 
+						message[i] == 'i' || message[i] == 'o' || 
+						message[i] == 'u' || message[i] == 'y')
+						{
+							nbvoyelle ++;			
+						}
 					}
-				}
-				tableau2= (char*)malloc((taille-nbvoyelle) * sizeof(char));
-				int j = 0;
-				for(int i =0; i< taille; i++)
-				{
-					if(message[i] != 'a' || message[i] != 'e' || 
-					message[i] != 'i' || message[i] != 'o' || 
-					message[i] != 'u' || message[i] != 'y')
+					tableau2= (char*)malloc((taille-nbvoyelle) * sizeof(char));
+					int j = 0;
+					for(int i =0; i< taille; i++)
 					{
-						tableau2[j] = message[i];
-						j++;				
+						if(message[i] != 'a' || message[i] != 'e' || 
+						message[i] != 'i' || message[i] != 'o' || 
+						message[i] != 'u' || message[i] != 'y')
+						{
+							tableau2[j] = message[i];
+							j++;				
+						}
 					}
-				}
-				taille = taille-nbvoyelle;
+					taille = taille-nbvoyelle;
+					printf("2");
 				break;
 			
 			}
 			
 			//Ecriture du message modifié
-			for(int i =0; i< taille; i++)
+			/*for(int i =0; i< taille; i++)
 			{
-				fputc(tableau2[i],f);
-			}
-			
+				fputs(tableau2,f);
+			}*/
+			//fputs("salut",f);
+			printf("\ntransformation : %s", tableau2);
+			fputs(tableau2,f);
 			//Fermeture fichier et pipe
 			fclose(f);
 			close(tube[0]);		
